89dd4a2cb0d7a2a8d98db310acf72e5624a062b7
Gabriel Grubba <gabriel@discourse.org>
2024-06-06 17:52:13 -0300

    DEV: Make tests more generic

--- spec/system/custom_category_group_spec.rb
+++ spec/system/custom_category_group_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "Testing Category Groups Theme Component", system: true do
   let!(:theme_component) { upload_theme_component }
-  let!(:lounge_category) { Fabricate(:category, name: "lounge") }
+  let!(:category) { Fabricate(:category) }
 
   class ExampleGroupLink
     def self.id
@@ -21,7 +21,7 @@ RSpec.describe "Testing Category Groups Theme Component", system: true do
       [
         {
           "id" => ExampleGroupLink.id,
-          "url" => "/c/lounge",
+          "url" => "/c/#{category.slug}",
           "color" => "#ff0000",
           "title" => ExampleGroupLink.title,
           "description" => ExampleGroupLink.description,
@@ -32,7 +32,7 @@ RSpec.describe "Testing Category Groups Theme Component", system: true do
     theme_component.update_setting(:fancy_styling, true)
     theme_component.update_setting(
       :category_groups,
-      "Default Categories: #{ExampleGroupLink.id}, lounge",
+      "Default Categories: #{ExampleGroupLink.id}, #{category.slug}",
     )
     SiteSetting.desktop_category_page_style = "categories_boxes"
     theme_component.save!
@@ -44,6 +44,6 @@ RSpec.describe "Testing Category Groups Theme Component", system: true do
     expect(page).to have_text ExampleGroupLink.title
     expect(page).to have_text ExampleGroupLink.description
     find(".extra-link-#{ExampleGroupLink.id}").click
-    expect(page).to have_text "lounge topics"
+    expect(page).to have_text "#{category.name} topics"
   end
 end
